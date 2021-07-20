a569df047ec7b6a3a44ace7620ab20c1e33e9f39
Simon Schall <simon-1.schall@uni-ulm.de>
2021-07-20 09:39:09 +0200

    add subtraction

--- ProfCalculator/src/de/uulm/sp/swt/profcalculator/ProfCalculator.java
+++ ProfCalculator/src/de/uulm/sp/swt/profcalculator/ProfCalculator.java
@@ -5,6 +5,7 @@ import de.uulm.sp.swt.profcalculator.expressions.CounterValue;
 import de.uulm.sp.swt.profcalculator.expressions.Expression;
 import de.uulm.sp.swt.profcalculator.expressions.Multiplication;
 import de.uulm.sp.swt.profcalculator.expressions.NecessaryBrackets;
+import de.uulm.sp.swt.profcalculator.expressions.Subtraction;
 import de.uulm.sp.swt.profcalculator.expressions.Value;
 import de.uulm.sp.swt.profcalculator.gui.BlueFontGUIFactory;
 import de.uulm.sp.swt.profcalculator.gui.GUIFactory;
@@ -32,6 +33,7 @@ public class ProfCalculator	extends Application implements EventHandler<ActionEv
 
 	private Button additionButton = guiFactory.createButton("+");
 	private Button multiplicationButton = guiFactory.createButton("*");
+	private Button subtractionButton = new Button("-");
 
 	private Label resultLabel = guiFactory.createLabel();
 
@@ -40,7 +42,7 @@ public class ProfCalculator	extends Application implements EventHandler<ActionEv
 		stage.setTitle("Professorial Calculator");
 		errorLabel.setTextFill(Color.web("#AA0000"));
 
-		VBox layout = new VBox(10, errorLabel, inputField, additionButton, multiplicationButton, resultLabel);
+		VBox layout = new VBox(10, errorLabel, inputField, additionButton, subtractionButton, multiplicationButton, resultLabel);
 		layout.setPadding(new Insets(20, 80, 20, 80));
 		Scene scene = new Scene(layout);
 
@@ -48,6 +50,7 @@ public class ProfCalculator	extends Application implements EventHandler<ActionEv
 		stage.show();
 		additionButton.setOnAction(this);
 		multiplicationButton.setOnAction(this);
+		subtractionButton.setOnAction(this);
 		updateGUI();
 	}
 
@@ -62,6 +65,9 @@ public class ProfCalculator	extends Application implements EventHandler<ActionEv
 			else if (event.getSource() == multiplicationButton) {
 				expression = new Multiplication(expression, new Value(newValue));
 				Logger.getLogger().log("* " + newValue);
+			}
+			else if (event.getSource() == subtractionButton) {
+				expression = new Subtraction(expression, new Value(newValue));
 			}
 			expression = new NecessaryBrackets(expression);
 			updateGUI();
